e940fc62f96a4b027274cbb27a82e9c8fa5edb2a
Matthew <m_kmorgan@hotmail.com>
2021-08-12 13:58:59 -0600

    finished the human class object

--- Human.py
+++ Human.py
@@ -1,55 +1,66 @@
-class Human:
+class Human():
     """Can start with an input of (name, age, children, parents) """
     name = ''
     age = 0
-    children = []
-    parents = []
 
-    def __init__(self, input_name=name, input_age=age, input_children=children, input_parents=parents):
-        """created parent child connections if not already established"""
-        if len(input_children) > 0:
-            for child in input_children:
-                if self not in child.parents:
-                    child.add_parent(self)
-        if len(input_parents) > 0:
-            for parent in input_parents:
-                if (self not in parent.children):
-                    parent.has_child(self)
+    def link_parents(self, parent):
+        parent.children.append(self)
 
+    def link_children(self, child):
+        child.parents.append(self)
+
+    def __init__(self, input_name=name, input_age=age):
+        self.children = []
+        self.parents = []
         self.name = input_name
         self.age = input_age
-        self.children = input_children
-        self.parents = input_parents
 
     def has_birthday(self):
         self.age += 1
-        print(f'Happy Birthday{self.name}! You are {self.age} years old!')
+        print(f'Happy Birthday {self.name}! You are {self.age} years old!')
 
-    def has_child(self, child):
-        if self.age < 18:
-            print(f'Sorry, {self.name} is too young to have kids')
-        else:
-            self.children.append(child)
-            child.add_parent(self)
+    def add_child(self, child):
+        self.children.append(child)
+        self.link_children(child)
 
     def add_parent(self, parent):
         self.parents.append(parent)
+        self.link_parents(parent)
 
     def get_age(self):
         print(f'{self.name} is {self.age} years old')
 
     def get_parents(self):
-        pass
-        # print(f'{self.name}parents are {self.parents[0].name}')
+        if len(self.parents) > 0:
+            names = ''
+            for parent in self.parents:
+                if names == '':
+                    names = parent.name
+                else:
+                    names += f' & {parent.name}'
+            print(f'{self.name}\'s parent(s): {names}.')
+        else:
+            print(f'{self.name} does not have any parents listed.')
+
+    def get_children(self):
+        if len(self.children) > 0:
+            names = ''
+            for child in self.children:
+                if names == '':
+                    names = child.name
+                else:
+                    names += f' & {child.name}'
+            print(f'{self.name}\'s children: {names}.')
+        else:
+            print(f'{self.name} does not have any children listed.')
 
 
 bob = Human('Bob', 1)
-sally = Human('Sally', 22, [bob])
+sally = Human('Sally', 22)
+sally.add_child(bob)
 bob.has_birthday()
-bob.get_age()
-sally.get_age()
 sally.has_birthday()
 bob.get_parents()
-
-print(sally.children[0].name)
-print(bob.parents[0].name)
+sally.get_parents()
+bob.get_children()
+sally.get_children()
