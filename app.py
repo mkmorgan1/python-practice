55c302ebe98c7fd34c634e7ba2b96222a5161283
Matthew <m_kmorgan@hotmail.com>
2021-08-13 11:23:17 -0600

    created a route and imported a function

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from testFunc import add
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Flask App"
+    return f"{add(1, 2)}"
+
+
+@app.route("/sup")
+def sup():
+    return "Sup"
