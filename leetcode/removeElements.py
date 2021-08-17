d43c4b8ffa1057cc327688a4d17c8157b6d6ec65
Matthew <m_kmorgan@hotmail.com>
2021-08-16 22:35:27 -0600

    finished removeElements

---
+++ leetcode/removeElements.py
@@ -0,0 +1,14 @@
+# Given an integer array nums and an integer val, remove all occurrences of val in nums in-place.
+
+class Solution:
+    def removeElement(self, nums, val: int) -> int:
+        if val in nums:
+            nums.remove(val)
+            self.removeElement(nums, val)
+        else:
+            print(nums)
+            print(len(nums))
+
+
+test = Solution()
+test.removeElement([1, 2, 3, 4, 5, 5], 5)
